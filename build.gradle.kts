66a12fbdace7828570e4f2fd77d5a65b1eb63fb1
Renke Christian von Seggern <renke.von.seggern@eddyson.de>
2023-08-22 07:55:37 +0200

    build: setup project
    With this setup, the generateOpenApiDocs task is considered up-to-date,
    even if the controller source code has changed.

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  id("com.github.psxpaul.execfork") version "0.2.2"
+  id("io.spring.dependency-management") version "1.1.3"
+  id("java")
+  id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
+  id("org.springframework.boot") version "2.7.14"
+}
+
+group = "org.rechvs"
+version = "0.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.0")
+}
+
+tasks {
+  test {
+    useJUnitPlatform()
+  }
+}
+
+openApi {
+  outputDir = projectDir.resolve("openapi")
+  outputFileName = "my-openapi.json"
+}
